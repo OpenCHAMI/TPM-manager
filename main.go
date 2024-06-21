63c94c5e60f819076aa75063ac764a41a801a514
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-06-21 10:54:12 -0600

    Implement (blocking) Ansible playbook launching
    This currently uses os/exec's .Run(), which blocks until the command
    finishes. This works™ for now, but is very not ideal in the long term.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
 	"strings"
 	"sync"
@@ -29,6 +30,7 @@ func main() {
 	port := flag.Int("port", 27730, "port on which to listen for POSTs")
 	interval := flag.Duration("interval", 5*time.Minute, "how frequently to push tokens, regardless of buffer length")
 	batchSize := flag.Int("batch-size", 100, "how full the node buffer must be to trigger a non-timed push")
+	playbook := flag.String("playbook", "main.yaml", "Ansible playbook to run against nodes")
 	debug := flag.Bool("debug", false, "sets log level to debug")
 	flag.Parse()
 
@@ -53,7 +55,7 @@ func main() {
 	// Launch the HTTP and node-slice watchers
 	log.Info().Msgf("Awaiting POST requests on port %d...", *port)
 	go http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
-	go watchNodes(&nodes, *interval, *batchSize)
+	go watchNodes(&nodes, *interval, *batchSize, playbook)
 
 	// Exit cleanly when an OS signal is received
 	sigs := make(chan os.Signal, 1)
@@ -86,7 +88,7 @@ func respondNodePost(w http.ResponseWriter, r *http.Request, nodes *SafeUpdating
 	}
 }
 
-func watchNodes(nodes *SafeUpdatingSlice, interval time.Duration, batchSize int) {
+func watchNodes(nodes *SafeUpdatingSlice, interval time.Duration, batchSize int, playbook *string) {
 	timer := time.NewTicker(interval)
 
 	// Launch token push to current set of nodes, when either:
@@ -98,14 +100,14 @@ func watchNodes(nodes *SafeUpdatingSlice, interval time.Duration, batchSize int)
 			log.Info().Msg("Caught a buffer update!")
 			if nodeLen >= batchSize {
 				timer.Reset(interval)
-				doTokenPush(nodes)
+				runAnsiblePlaybook(playbook, nodes)
 			} else {
 				log.Debug().Msgf("Buffer now contains %d nodes; not launching yet", nodeLen)
 			}
 		case <-timer.C:
 			log.Info().Msg("Caught a timer tick!")
 			if len(nodes.slice) > 0 {
-				doTokenPush(nodes)
+				runAnsiblePlaybook(playbook, nodes)
 			} else {
 				log.Debug().Msg("No nodes in buffer; skipping launch")
 			}
@@ -113,17 +115,19 @@ func watchNodes(nodes *SafeUpdatingSlice, interval time.Duration, batchSize int)
 	}
 }
 
-func doTokenPush(nodes *SafeUpdatingSlice) {
+func runAnsiblePlaybook(playbook *string, nodes *SafeUpdatingSlice) {
 	log.Info().Msgf("Launching token push to %v", nodes.slice)
 
 	nodes.Lock()
 	// Compose our Ansible launch command, in exec form
 	// A trailing comma is necessary for a single node, and fine for multiple nodes
-	launchCmd := append([]string{"ansible-playbook", "main.yaml"}, "-i", strings.Join(nodes.slice, ",")+",")
+	ansibleArgs := []string{*playbook, "--inventory", strings.Join(nodes.slice, ",")+","}
 	// Clear node list, since we've launched the token push
 	nodes.slice = nil
 	nodes.Unlock()
 
-	// TODO: How do exec?
-	log.Debug().Msg("Would run: " + strings.Join(launchCmd, " "))
+	// Launch Ansible
+	log.Debug().Msgf("Launching Ansible with %v", ansibleArgs)
+	ansible := exec.Command("ansible-playbook", ansibleArgs...)
+	ansible.Run()
 }
